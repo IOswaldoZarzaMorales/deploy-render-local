f309746ca243ad7d7b9ff6efaed78dc810507324
AKMoralesRamos <dental.morak@gmail.com>
2024-02-16 02:55:59 -0600

    front-end

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,26 @@
+const loginForm = document.querySelector('#loginForm')
+const errorMessage = document.querySelector('#error-message');
+
+loginForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
+    const url = `https://loklapp.onrender.com/admin/users/byEmail?email=${email}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        if (data.correo === email && data.contrasena === password){
+            alert(`Bienvenido ${data.correo}`)
+            window.location.href = './index.html' 
+        } else if (data.correo === email && data.contrasena != password){
+            errorMessage.textContent = 'Usuario y/o contraseña incorrectos';
+        }
+    })
+    .catch(error => {
+        console.error(error, 'Usuario no encontrado');
+        errorMessage.textContent = 'Usuario y/o contraseña incorrectos';
+    })
+})
+
